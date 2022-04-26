230491e1c87ae25fec3b60846be2c2c256f119cb
Rafael C <rafa.rrcr@gmail.com>
2022-04-25 22:32:25 -0500

    pantalla de pago

--- docs/scripts/quoteFlow/issueRequest.js
+++ docs/scripts/quoteFlow/issueRequest.js
@@ -2,6 +2,8 @@ $(function () {
   initDatosContratante();
   initDatosConductor();
   initDatosBeneficiario();
+  initDatosNuevoContratante();
+  enableDisableNcontratante(true);
 
   function initDatosContratante() {
     $("#personaFisicaContratante").prop("checked", true);
@@ -50,4 +52,60 @@ $(function () {
       }
     });
   }
+
+  function initDatosNuevoContratante(params) {
+    $("#personaFisicaNuevoContratante").prop("checked", true);
+    $("#personaMoralNuevoContratanteDiv").hide();
+
+    //Switch tipo persona Beneficiario
+    $("input[type=radio][name=tipoPersonaNuevoContratante]").change(function () {
+      if (this.value == "personaFisica") {
+        $("#personaFisicaNuevoContratanteDiv").show();
+        $("#personaMoralNuevoContratanteDiv").hide();
+      } else if (this.value == "personaMoral") {
+        $("#personaFisicaNuevoContratanteDiv").hide();
+        $("#personaMoralNuevoContratanteDiv").show();
+      }
+    });
+  }
+
+
+ 
+    $("#switchNuevoContratante").click(function () {
+      var estado = !$("#switchNuevoContratante").is(":checked");
+      enableDisableNcontratante(estado);
+    });
+    
+    function enableDisableNcontratante(estado) {
+      $("#personaFisicaNuevoContratante").prop("disabled", estado);
+      $("#personaMoralNuevoContratante").prop("disabled", estado);
+      $("#nombreNContratante").prop("disabled", estado);
+      $("#aPaternoNContratante").prop("disabled", estado);
+      $("#aMaternoNContratante").prop("disabled", estado);
+      $("#fechaNacimientoNContratante").prop("disabled", estado);
+      $("#edadNContratante").prop("disabled", estado);
+      $("#sexoNContratante").prop("disabled", estado);
+      $("#rfcFisicaNContratante").prop("disabled", estado);
+      $("#homoclaveNContratante").prop("disabled", estado);
+      $("#razonSocialNContratante").prop("disabled", estado);
+      $("#fechaConstitucionNContratante").prop("disabled", estado);
+      $("#rfcMoralNContratante").prop("disabled", estado);
+      $("#representanteLegalNContratante").prop("disabled", estado);
+      $("#mailContactoNContratante").prop("disabled", estado);
+      $("#celularContactoNContratante").prop("disabled", estado);
+      $("#telefonoContactoNContratante").prop("disabled", estado);
+      $("#calleNContratante").prop("disabled", estado);
+      $("#numExteriorNContratante").prop("disabled", estado);
+      $("#numInteriorNContratante").prop("disabled", estado);
+      $("#coloniaNContratante").prop("disabled", estado);
+      $("#estadoNContratante").prop("disabled", estado);
+      $("#poblacionNContratante").prop("disabled", estado);
+      $("#cpNContratante").prop("disabled", estado);
+    }
+  
+    $("#btnFormaPago").click(function () {
+      location.href = "../quoteFlow/payment.html";
+    });
+
+
 });
